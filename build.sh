17d566202e9506da89589e1b4dd2cc6c26d15386
Darren <Darren_Serious.hotmail.com>
2023-10-24 14:52:30 +0000

    feat(openocd): add openocd and download success

--- build.sh
+++ build.sh
@@ -21,12 +21,22 @@ cmake_build() {
   cmake --build $build_path
 }
 
+binfile="../build/PRO_MCU.bin"
+openocd_program() {
+  cd tools
+  openocd -f openocd-jlink-swd.cfg -c "program $binfile"
+  cd ..
+}
+
 if [ $1 == "cmake" ]
 then
   cmake_build
 elif [ $1 == "docker" ]
 then
   docker_run
+elif [ $1 == "download" ]
+then
+  openocd_program
 else
   echo "没有符合的命令"
 fi
